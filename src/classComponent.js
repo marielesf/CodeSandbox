bc7edd07fb1cbf858b7446bf66a4921cd0ee067f
Mariele <mariele.fonttana@gmail.com>
2022-02-03 22:55:14 +0000

    component de classe

---
+++ src/classComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class HelloWorld extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="title">componente</h1>
+        <h2>de classe</h2>
+      </div>
+    );
+  }
+}
+export default HelloWorld;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import HelloWorld from "./classComponent";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
     <App />
+    <HelloWorld />
   </StrictMode>,
   rootElement
 );
